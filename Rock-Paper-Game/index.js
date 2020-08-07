08bdb093fdbfd432dd500154c4316dcd49b921e4
KingThq <1581563341@qq.com>
2020-08-07 17:01:52 +0800

    feat: add rock-paper game

---
+++ Rock-Paper-Game/index.js
@@ -0,0 +1,25 @@
+// process.argv 可以记录用户输入的内容
+var playerAction = process.argv[process.argv.length - 1];
+console.log(playerAction)
+
+var random = Math.random() * 3;
+
+if (random < 1) {
+  var computerAction = 'rock';
+} else if (random > 2) {
+  var computerAction = 'scissor';
+} else {
+  var computerAction = 'paper';
+}
+
+if (playerAction === computerAction) {
+  console.log('平局');
+} else if (
+  (computerAction === 'rock' && playerAction === 'paper') ||
+  (computerAction === 'scissor' && playerAction === 'rock') ||
+  (computerAction === 'paper' && playerAction === 'scissor')
+) {
+  console.log('你赢了');
+} else {
+  console.log('你输了');
+}
